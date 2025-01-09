ee469f3315daffa2f9c735bb98e3bc17b9ad4852
g.litvin <litgleb0@gmail.com>
2025-01-09 20:41:45 +0200

    added copy/paste functionality

---
+++ Calc/Presentation Layer/UIKit Subclasses/LCDDisplay.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class LCDDisplay: UIView {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet var label: UILabel!
+    
+    // MARK: - Initialisers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    private func sharedInit() {
+        
+    }
+}
